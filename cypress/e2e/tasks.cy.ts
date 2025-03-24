7126befed968a20d57429a25d6fef9ecdde0c348
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-03-25 00:45:35 +0300

    feat: CFN-4 add not stable login workflow

--- cypress/e2e/tasks.cy.ts
+++ cypress/e2e/tasks.cy.ts
@@ -2,7 +2,28 @@ import { levels } from '../fixtures/levels';
 
 console.log(levels);
 
-describe.skip('...', () => {
+describe('...', () => {
+  it('pretest', () => {
+    cy.visit('https://codingfantasy.com');
+    cy.get('a[href="/login"]').click();
+    cy.get('input[name="email"]').type(Cypress.env('email_login'));
+    cy.get('button').click();
+    cy.get('div[role="dialog"]').should('contain', 'Check your email!');
+    cy.origin('https://accounts.zoho.eu', () => {
+      cy.visit('/signin');
+      cy.get('input[name="LOGIN_ID"]').type(Cypress.env('email_login'));
+      cy.get('#nextbtn').click();
+      cy.get('input[name="PASSWORD"]').type(Cypress.env('email_password'));
+      cy.get('#nextbtn').click();
+    });
+
+    cy.origin('https://mail.zoho.eu/zm', () => {
+      cy.get(
+        '[aria-label*="noreply@codingfantassy-prod.firebaseapp.com"]:first'
+      ).click();
+    });
+  });
+
   it('Should open page', () => {
     cy.visit('https://codingfantasy.com');
     cy.get('h1').should('contain', 'Learn Code By Playing Games');
