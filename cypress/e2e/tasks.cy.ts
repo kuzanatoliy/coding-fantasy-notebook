69a9af3f870a30f60b60b093dc758cd7af4e3cad
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-03-26 23:22:08 +0300

    feat: CFN-4 attempt to use rambler

--- cypress/e2e/tasks.cy.ts
+++ cypress/e2e/tasks.cy.ts
@@ -9,32 +9,29 @@ describe('...', () => {
     cy.get('input[name="email"]').type(Cypress.env('email_login'));
     cy.get('button').click();
     cy.get('div[role="dialog"]').should('contain', 'Check your email!');
-    cy.visit('https://accounts.zoho.eu/signin');
-    cy.origin('https://accounts.zoho.eu', () => {
+    cy.visit('https://mail.rambler.ru/folder/INBOX');
+    //cy.origin('https://www.rambler.ru', () => {
+    //  cy.get('[href="https://mail.rambler.ru/"]').click();
+    //});
+    cy.visit(
+      'https://id.rambler.ru/login-20/login?back=https%3A%2F%2Fmail.rambler.ru%2F&rname=mail&theme=mail&session=false'
+    );
+    cy.origin('https://id.rambler.ru', () => {
       Cypress.on('uncaught:exception', () => false);
-
-      cy.get('input[name="LOGIN_ID"]').type(Cypress.env('email_login'));
-      cy.get('#nextbtn').click();
-      cy.get('input[name="PASSWORD"]').type(Cypress.env('email_password'));
-      cy.get('#nextbtn').click();
+      cy.get('#login').type(Cypress.env('email_login'));
+      cy.get('#password').type(Cypress.env('email_password'));
+      cy.get('button[type="submit"]').click();
       cy.get('body').then(($body) => {
-        if ($body.find('.announcement_header').length) {
-          cy.get('.remind-later').click();
+        if ($body.find('#phone').length) {
+          cy.get('footer botton').click();
         }
       });
     });
-
-    cy.visit('https://mail.zoho.eu/zm');
-
-    cy.origin('https://mail.zoho.eu/', () => {
-      cy.get('[aria-label="Notification"]').click();
-      cy.get(
-        '[aria-label*="noreply@codingfantassy-prod.firebaseapp.com"]:first-of-type'
-      ).click();
-      cy.get(
-        'a[href*="https://codingfantassy-prod.firebaseapp.com/__/auth"]'
-      ).click();
-    });
+    //cy.visit('https://mail.rambler.ru');
+    //cy.origin('https://mail.rambler.ru', () => {
+    //  cy.get('a[href="/folder/INBOX"]').click();
+    //  cy.get('.ListItem-root-1i:first-of-type').click();
+    //});
 
     cy.visit('https://codingfantasy.com/games');
     cy.get('a[href="/profile"]').should('contain', 'Hero');
