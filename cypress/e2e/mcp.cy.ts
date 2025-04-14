0bf81aa32520335d14c8d674c2cf46619d2bea79
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-04-15 00:46:05 +0300

    feat: CFN-12 add third act

--- cypress/e2e/mcp.cy.ts
+++ cypress/e2e/mcp.cy.ts
@@ -107,5 +107,53 @@ describe('MCP', () => {
     cy.wait(7000);
     cy.contains('Yep. Time to get back to work.');
     cy.get('button').contains('Next Act').click();
+    cy.get('h2').should('contain', 'Act 3/5: Prompt Engineering');
+    cy.wait(7000);
+    cy.contains('TODO.md');
+    cy.contains('Similar concept, but more structured.');
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(20000);
+    cy.contains('(unimpressed) Show me.');
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(17000);
+    cy.contains(
+      "Never mind. Let's focus on implementing the Prompts capability. That's the next piece we need."
+    );
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(11000);
+    cy.contains(
+      'Exactly. Instead of clients having to know exactly how to format'
+    );
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(17000);
+    cy.contains("That's why standardization matters. Without MCP,");
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(10000);
+    cy.contains("Let's build it. What's the implementation look like?");
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(4000);
+    cy.contains(
+      'These examples are fantastic. Now I think I understand how to build a prompts server.'
+    );
+    cy.contains('prompts-server.js');
+    cy.contains('prompts-client.js');
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(2000);
+    cy.contains('Time for your coding challenge!');
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(7000);
+    cy.contains(
+      "For now, yes. We're simulating what the interface would look like."
+    );
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(12000);
+    cy.contains('Chase exits, muttering about needing a stronger microdose');
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(18000);
+    cy.contains('Steve opens his laptop and starts typing');
+    cy.get('button').contains('Next Scene').click();
+    cy.wait(7000);
+    cy.contains('Both you and Steve pretend not to have seen it.');
+    cy.get('button').contains('Next Act').click();
   });
 });
